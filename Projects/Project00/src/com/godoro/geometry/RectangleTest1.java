8628986bfa9cb4159af993ed26b1cb4f258f6ad1
Burak YILDIZ <burak@yaztem.com>
2022-09-04 02:10:52 +0300

    Refactor rectangle test class

--- Projects/Project00/src/com/godoro/geometry/RectangleTest1.java
+++ Projects/Project00/src/com/godoro/geometry/RectangleTest1.java
@@ -2,6 +2,10 @@ package com.godoro.geometry;
 
 public class RectangleTest1 {
     public static void main(String[] args) {
-        System.out.println("Alan: " + 3 * 4);
+        int width = 3;
+        int height = 4;
+        int area = width * height;
+        
+        System.out.println("Alan: " + area);
     }
 }
