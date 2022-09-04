154b6c61b02a8f370babaff692ace54e6647efa8
Burak YILDIZ <burak@yaztem.com>
2022-09-04 22:00:05 +0300

    Add supplier class

--- Projects/Project01/src/com/ibtech/inheritance/Test.java
+++ Projects/Project01/src/com/ibtech/inheritance/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Customer customer = new Customer(401, "Neşet", "Ertaş", 3450);
         Person person = new Customer(402, "Mahsuni", "Şerif", 6550);
+        Supplier supplier = new Supplier(701, "Orhan", "Gencebay", 8125);
 
     }
 }
