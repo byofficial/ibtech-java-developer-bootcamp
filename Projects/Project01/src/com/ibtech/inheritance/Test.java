cdadbb7d1a4f5b6b6fb730d1751b4b778ff02111
Burak YILDIZ <burak@yaztem.com>
2022-09-04 21:00:55 +0300

    Add customer and person class

---
+++ Projects/Project01/src/com/ibtech/inheritance/Test.java
@@ -0,0 +1,11 @@
+package com.ibtech.inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.setCustomerId(401);
+        customer.setFirstName("Neşet");
+        customer.setLastName("Ertaş");
+        customer.setTotalDebit(3450);
+    }
+}
