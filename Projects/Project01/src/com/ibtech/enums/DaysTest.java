5fc9816fe0a453bb519e5639558281886bd90df1
Burak YILDIZ <burak@yaztem.com>
2022-09-08 01:14:24 +0300

    Add daysTest class

---
+++ Projects/Project01/src/com/ibtech/enums/DaysTest.java
@@ -0,0 +1,18 @@
+package com.ibtech.enums;
+
+public class DaysTest {
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Paartesi");
+                break;
+            case 2:
+                System.out.println("Salı");
+                break;
+            case 3:
+                System.out.println("Çarşamba");
+                break;
+        }
+    }
+}
