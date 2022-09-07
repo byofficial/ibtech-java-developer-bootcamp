941231c577b3d8032a57501d3fd838f3ee7ac08c
Burak YILDIZ <burak@yaztem.com>
2022-09-08 01:18:50 +0300

    Refactor final static variable days for daysTest class

--- Projects/Project01/src/com/ibtech/enums/DaysTest.java
+++ Projects/Project01/src/com/ibtech/enums/DaysTest.java
@@ -1,6 +1,10 @@
 package com.ibtech.enums;
 
 public class DaysTest {
+    public final static int MONDAY = 1;
+    public final static int TUESDAY = 2;
+    public final static int WEDNESDAY = 3;
+
     private static void printDay(int day) {
         switch (day) {
             case 1:
@@ -12,11 +16,13 @@ public class DaysTest {
             case 3:
                 System.out.println("Çarşamba");
                 break;
+            default:
+                System.out.println("Geçersiz gün düzgüsü");
         }
     }
 
     public static void main(String[] args) {
-        int day = 2;
+        int day = TUESDAY;
         printDay(day);
     }
 }
