491fa262b612baa1e241dec3cd3cc2c958d6375f
Burak YILDIZ <burak@yaztem.com>
2022-09-08 01:58:52 +0300

    Add console formatter class

---
+++ Projects/Project01/src/com/ibtech/console/FormatterTest.java
@@ -0,0 +1,10 @@
+package com.ibtech.console;
+
+public class FormatterTest {
+    public static void main(String[] args) {
+        long l = 3;
+        String s = "Godoro";
+        double d = 3.1415;
+        System.out.printf("Uzun: %d %s %f\n", l, s, d);
+    }
+}
