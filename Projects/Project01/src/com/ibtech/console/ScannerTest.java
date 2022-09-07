9c7e74fa517a5ed6a8709f33825b9b8c9b8d5574
Burak YILDIZ <burak@yaztem.com>
2022-09-08 02:05:59 +0300

    Add scanner test class

---
+++ Projects/Project01/src/com/ibtech/console/ScannerTest.java
@@ -0,0 +1,23 @@
+package com.ibtech.console;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Uzun: ");
+        long l = scanner.nextLong();
+
+        System.out.println("Sicim: ");
+        String s = scanner.next();
+
+        System.out.println("İkişer: ");
+        double d = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.printf("Uzun: %10d Sicim: %-20s İkişer: %11.2f\n", l, s, d);
+
+    }
+}
