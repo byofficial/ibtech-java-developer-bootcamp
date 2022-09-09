48291fac415866cb5bb2d6bfa03c270633cd0f4d
Burak YILDIZ <burak@yaztem.com>
2022-09-09 20:31:31 +0300

    Add thread test example

---
+++ Projects/Project02/src/com/ibtech/thread/MyThread.java
@@ -0,0 +1,15 @@
+package com.ibtech.thread;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Koşuyor...");
+                Thread.sleep(500);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
