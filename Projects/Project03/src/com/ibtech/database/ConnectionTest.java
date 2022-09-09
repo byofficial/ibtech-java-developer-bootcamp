f2e7d0365d41a7b56c95e20361f85907edd5b8fc
Burak YILDIZ <burak@yaztem.com>
2022-09-10 00:16:36 +0300

    Add postgresql connection test class

---
+++ Projects/Project03/src/com/ibtech/database/ConnectionTest.java
@@ -0,0 +1,21 @@
+package com.ibtech.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionTest {
+    public static void main(String[] args) throws Exception {
+
+        String url = "jdbc:postgresql://localhost/dbibtech";
+        String user = "postgres";
+        String password = "root";
+
+        String driver = "org.postgresql.Driver";
+        Class.forName(driver);
+
+        Connection connection = DriverManager.getConnection(url, user, password);
+        System.out.println(connection.getMetaData().getDatabaseProductName());
+        connection.close();
+
+    }
+}
