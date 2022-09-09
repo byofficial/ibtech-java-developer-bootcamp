b56c5f81d64fedae239dedce224b6527b67b08f2
Burak YILDIZ <burak@yaztem.com>
2022-09-10 00:57:21 +0300

    Add transaction test class

---
+++ Projects/Project03/src/com/ibtech/database/TransactionTest.java
@@ -0,0 +1,42 @@
+package com.ibtech.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class TransactionTest {
+    public static void main(String[] args) throws Exception {
+
+        String url = "jdbc:postgresql://localhost/dbibtech";
+        String user = "postgres";
+        String password = "root";
+
+        String driver = "org.postgresql.Driver";
+        Class.forName(driver);
+
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        String sql = "insert into Product(productName,salesPrice) values(?,?)";
+
+        PreparedStatement statement1 = connection.prepareStatement(sql);
+        statement1.setString(1, "Ürün 101");
+        statement1.setDouble(2, 3010);
+        int affected1 = statement1.executeUpdate();
+
+        PreparedStatement statement2 = connection.prepareStatement(sql);
+        statement2.setString(1, "Ürün 102");
+        statement2.setDouble(2, 3020);
+        int affected2 = statement2.executeUpdate();
+
+
+        PreparedStatement statement3 = connection.prepareStatement(sql);
+        statement3.setString(1, "Ürün 103");
+        statement3.setDouble(2, 3030);
+        int affected3 = statement3.executeUpdate();
+
+        connection.close();
+
+        System.out.println("Etkilenmiş " + affected1 + " " + affected2 + " " + affected3);
+
+    }
+}
