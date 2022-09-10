9a1730ed4addc088fc68bdb7dec2da9845485dc0
Burak YILDIZ <burak@yaztem.com>
2022-09-10 23:13:32 +0300

    Add xml utilities class

--- Projects/Project05/src/main/java/com/ibtech/xml/XmlBuild.java
+++ Projects/Project05/src/main/java/com/ibtech/xml/XmlBuild.java
@@ -3,25 +3,23 @@ package com.ibtech.xml;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
 public class XmlBuild {
+
     public static void main(String[] args) throws Exception {
 
+        //JAXP
         String path = "C:\\Users\\burak\\IdeaProjects\\ExPath\\Product.xml";
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(path);
+        Document document = XmlUtilities.parse(path);
 
+        //DOM
         Element product = document.getDocumentElement();
-        String id = product.getAttribute("id");
+        long id = XmlUtilities.getAttribute(product, "id", 0);
         System.out.println("Özdeşlik: " + id);
 
-        String name = product.getElementsByTagName("name").item(0).getTextContent();
+        String name = XmlUtilities.getSingleElementText(product, "name", "");
         System.out.println("Ad: " + name);
 
-        String price = product.getElementsByTagName("price").item(0).getTextContent();
-        System.out.println("Ad: " + price);
+        double price = XmlUtilities.getSingleElementText(product, "price", 0);
+        System.out.println("Eder: " + price);
     }
 }
