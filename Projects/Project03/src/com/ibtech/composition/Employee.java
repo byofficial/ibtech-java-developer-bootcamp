209cdb40de93827698540a2814c31a87a61248e2
Burak YILDIZ <burak@yaztem.com>
2022-09-10 03:43:46 +0300

    Add employee entity

---
+++ Projects/Project03/src/com/ibtech/composition/Employee.java
@@ -0,0 +1,40 @@
+package com.ibtech.composition;
+
+public class Employee {
+    private long employeeId;
+    private String employeeName;
+    private double monthlySalary;
+
+    public Employee(long employeeId, String employeeName, double monthlySalary) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.monthlySalary = monthlySalary;
+    }
+
+    public Employee() {
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+}
