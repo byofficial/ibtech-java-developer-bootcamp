b20fa096ea94cc1d4f2a60ace7699034e5602ae4
Burak YILDIZ <burak@yaztem.com>
2022-09-11 23:52:46 +0300

    Add employee insert test class use hibernate

---
+++ Projects/Project06/src/main/java/com/ibtech/orm/EmployeeInsert.java
@@ -0,0 +1,22 @@
+package com.ibtech.orm;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EmployeeInsert {
+    public static void main(String[] args) {
+        Employee employee = new Employee(0, "Müslüm Gürses", 13450);
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("MyPersistenceUnit");
+
+        EntityManager manager = factory.createEntityManager();
+        manager.getTransaction().begin();
+        manager.persist(employee);
+        manager.getTransaction().commit();
+        manager.close();
+        factory.close();
+
+        System.out.println("Özdeşlik: " + employee.getEmployeeId());
+    }
+}
