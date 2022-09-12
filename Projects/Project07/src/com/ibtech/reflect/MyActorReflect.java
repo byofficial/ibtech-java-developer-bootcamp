a2b2cb85f97e3259a196220d3833fde512b194cd
Burak YILDIZ <burak@yaztem.com>
2022-09-12 22:22:57 +0300

    Add actor reflect test class

---
+++ Projects/Project07/src/com/ibtech/reflect/MyActorReflect.java
@@ -0,0 +1,35 @@
+package com.ibtech.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+public class MyActorReflect {
+    public static void main(String[] args)
+            throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
+            InstantiationException, IllegalAccessException {
+
+        Class<?> clazz = Class.forName("com.ibtech.source.MyActor");
+        Constructor<?> constructor = clazz.getConstructor();
+        Object object = constructor.newInstance();
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            System.out.print(method.getReturnType().getName() + " ");
+            System.out.print(method.getName() + " ");
+            for (Parameter parameter : method.getParameters()) {
+                System.out.print(parameter.getType() + " " + parameter.getName() + ",");
+            }
+            System.out.println();
+
+            Method processMethod = clazz.getMethod("process");
+            processMethod.invoke(object);
+
+            Class<?>[] parameterTypes = {int.class, String.class, double.class};
+            Method transformMethod = clazz.getMethod("transform", parameterTypes);
+            Object[] arguments = {123, "Godoro", 98.76};
+            Object result = transformMethod.invoke(object, arguments);
+            System.out.println("Sonuç " + result);
+        }
+    }
+}
--- Projects/Project07/src/com/ibtech/source/MyActor.java
+++ Projects/Project07/src/com/ibtech/source/MyActor.java
@@ -2,12 +2,12 @@ package com.ibtech.source;
 
 public class MyActor {
     public String transform(int yourInt, String yourString, double yourDouble) {
-        return Integer.toString(yourInt) + " " + yourString + " " + Double.toString(yourDouble);
+        return Integer.toString(yourInt) + " - " + yourString + " - " + Double.toString(yourDouble);
     }
 
     public void consume(int yourInt, String yourString, double yourDouble) {
-        System.out.println(Integer.toString(yourInt) + " "
-                + yourString + " "
+        System.out.println(Integer.toString(yourInt) + " - "
+                + yourString + " - "
                 + Double.toString(yourDouble));
     }
 
